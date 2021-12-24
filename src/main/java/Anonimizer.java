a8385517d3819b4b3220493375e0cc2092fc7302
Bakhtin Igor <skilful2000@yandex.ru>
2021-12-25 02:46:21 +0300

    Scripted auto-commit on change (2021-12-25 02:46:21) by gitwatch.sh

--- src/main/java/Anonimizer.java
+++ src/main/java/Anonimizer.java
@@ -4,10 +4,14 @@ import akka.actor.ActorSystem;
 import akka.actor.Props;
 import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
+import akka.http.javadsl.server.Route;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
+
+import java.util.concurrent.CompletionStage;
 
 public class Anonimizer {
 
@@ -26,4 +30,6 @@ public class Anonimizer {
         System.in.read();
         binding.thenCompose(ServerBinding::unbind).thenAccept(unbound -> system.terminate());
     }
+
+    public static Route route(){}
 }
