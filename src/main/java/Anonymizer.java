2f919cfdc02021e37e3cc3b43caf13298a6fd438
Bakhtin Igor <skilful2000@yandex.ru>
2021-12-25 01:41:34 +0300

    Scripted auto-commit on change (2021-12-25 01:41:34) by gitwatch.sh

--- src/main/java/Anonymizer.java
+++ src/main/java/Anonymizer.java
@@ -1,6 +1,8 @@
+import akka.actor.ActorSystem;
+
 public class Anonymizer {
 
     public static void main(String[] args) {
-        
+        ActorSystem system = ActorSystem.create("routes");
     }
 }
