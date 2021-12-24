094d33f77e45f673b3cbebc720aae37cf69349ce
Bakhtin Igor <skilful2000@yandex.ru>
2021-12-25 02:42:56 +0300

    Scripted auto-commit on change (2021-12-25 02:42:56) by gitwatch.sh

---
+++ src/main/java/Anonimizer.java
@@ -0,0 +1,26 @@
+import akka.NotUsed;
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.http.javadsl.ConnectHttp;
+import akka.http.javadsl.Http;
+import akka.stream.ActorMaterializer;
+
+public class Anonimizer {
+
+    public static void main(String[] args) {
+        ActorSystem system = ActorSystem.create("routes");
+        ActorRef cachingActor = system.actorOf(Props.create());
+        Http http = Http.get(system);
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = createFlow(http, system, materializer, actor);
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                routeFlow,
+                ConnectHttp.toHost(HOST, PORT),
+                materializer
+        );
+        l.info("Server online at http://{}:{}/\n", HOST, PORT);
+        System.in.read();
+        binding.thenCompose(ServerBinding::unbind).thenAccept(unbound -> system.terminate());
+    }
+}
