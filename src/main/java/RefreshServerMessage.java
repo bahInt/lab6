3826e4ed8e89312f6d893d99f8d4731c80f4bdf4
Bakhtin Igor <skilful2000@yandex.ru>
2021-12-25 03:55:41 +0300

    Scripted auto-commit on change (2021-12-25 03:55:41) by gitwatch.sh

--- src/main/java/RefreshServerMessage.java
+++ src/main/java/RefreshServerMessage.java
@@ -2,5 +2,12 @@ import java.util.ArrayList;
 
 public class RefreshServerMessage {
     private final ArrayList<String> servers;
-    
+
+    public RefreshServerMessage(ArrayList<String> servers) {
+        this.servers = servers;
+    }
+
+    public ArrayList<String> getServers() {
+        return servers;
+    }
 }
