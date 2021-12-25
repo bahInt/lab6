06a6e4321f40aeda53962d4394765fd86884fe24
Bakhtin Igor <skilful2000@yandex.ru>
2021-12-25 07:16:49 +0300

    Scripted auto-commit on change (2021-12-25 07:16:49) by gitwatch.sh

--- src/main/java/Anonimizer.java
+++ src/main/java/Anonimizer.java
@@ -57,7 +57,8 @@ public class Anonimizer {
             parameter("url", url ->
                     parameter("count", count -> {
                         if(Integer.parseInt(count) <= 0) return completeWithFuture(fetch(url));
-                    return completeWithFuture(Patterns.ask(configStorageActor, new GetNextServer(), timeout));
+                    return completeWithFuture(Patterns.ask(configStorageActor, new GetNextServer(), timeout))
+                            .thenApply;
                     }))
         ));
     }
